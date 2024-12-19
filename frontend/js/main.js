e9426d65237762d7ca0af92af85050ee8ab406c0
abdalla-fayez <af.ragheb98@gmail.com>
2024-12-19 12:11:23 +0200

    Cleaned up main.js code

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,3 @@
-// Initialize variables for DOM elements
-// const departureSelect = document.getElementById('departure');
-// const arrivalSelect = document.getElementById('arrival');
-// const dateInput = document.getElementById('date');
-// const searchButton = document.getElementById('searchTrips');
-// const tripIdInput = document.getElementById('tripId');
-// const numSeatsInput = document.getElementById('numSeats');
-// const bookButton = document.getElementById('bookTrip');
-// const bookingStatus = document.getElementById('bookingStatus');
-// const errorAlert = document.getElementById('errorAlert'); // Alert container
-// const errorMessage = document.getElementById('errorMessage'); // Alert text
-// const tripsResults = document.getElementById('tripsResults'); // Trips container
 const departureSelect = document.getElementById('fromLocation');
 const arrivalSelect = document.getElementById('toLocation');
 const dateInput = document.getElementById('tripDate');
@@ -22,7 +10,6 @@ const tripTemplate = document.getElementById('tripTemplate'); // HTML template
 // Event listeners
 document.addEventListener('DOMContentLoaded', populateDropdowns);
 searchButton.addEventListener('click', searchTrips);
-// bookButton.addEventListener('click', redirectToPayment);
 
 // Function to populate dropdowns dynamically
 async function populateDropdowns() {
@@ -141,35 +128,3 @@ async function searchTrips() {
         errorAlert.classList.remove('d-none'); // Show the alert
     }
 }
-
-
-// // Function to redirect to payment
-// async function redirectToPayment() {
-//     const tripId = tripIdInput.value;
-//     const seatsBooked = numSeatsInput.value;
-
-//     if (!tripId || !seatsBooked || seatsBooked <= 0) {
-//         alert('Please provide valid Trip ID and Number of Seats!');
-//         return;
-//     }
-
-//     try {
-//         // Store trip details in the backend session
-//         const response = await fetch('/api/session/booking/store', {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify({ tripId, seatsBooked }),
-//         });
-
-//         if (response.ok) {
-//             // Redirect to payment.html without exposing details
-//             window.location.href = 'checkout.html';
-//         } else {
-//             console.error('Failed to store session data');
-//             alert('An error occurred while processing your request.');
-//         }
-//     } catch (error) {
-//         console.error('Error redirecting to payment:', error);
-//         alert('An error occurred while redirecting to the payment page.');
-//     }
-// }
