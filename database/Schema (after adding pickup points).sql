a5c422d5f60386af6bb2b1634ed97b413ca6c490
abdalla-fayez <af.ragheb98@gmail.com>
2025-01-05 12:40:49 +0200

    DB update for pick up points

--- database/Schema (after adding pickup points).sql
+++ database/Schema (after adding pickup points).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2025 at 02:57 PM
+-- Generation Time: Jan 05, 2025 at 11:39 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
